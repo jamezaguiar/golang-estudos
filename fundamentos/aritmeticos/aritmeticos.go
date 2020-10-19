7411f7e2bbd3c7545d64dccd26f63afbdb6e00b4
Jamerson Aguiar <jamersonalsilva14@gmail.com>
2020-10-19 17:57:22 -0300

    Operadores aritméticos

---
+++ fundamentos/aritmeticos/aritmeticos.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	a := 3
+	b := 2
+
+	fmt.Println("Soma =>", a+b)
+	fmt.Println("Subtração =>", a-b)
+	fmt.Println("Multiplicação =>", a*b)
+	fmt.Println("Divisão =>", a/b)
+	fmt.Println("Módulo =>", a%b)
+
+	// Algumas operações com math
+	c := 3.0
+	d := 2.0
+
+	fmt.Println("Maior =>", math.Max(c, d))
+	fmt.Println("Menor =>", math.Min(c, d))
+	fmt.Println("Exponenciação =>", math.Pow(c, d))
+}
