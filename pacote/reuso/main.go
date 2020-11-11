f4f845a397e946475239693f830f1b7e223ddb12
Jamerson Aguiar <jamersonalsilva14@gmail.com>
2020-11-11 10:27:37 -0300

    Uso de pacotes do GOPATH

---
+++ pacote/reuso/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/jamezaguiar/area"
+)
+
+func main() {
+	fmt.Println(area.Circ(6.0))
+	fmt.Println(area.Rect(5.0, 2.0))
+	fmt.Println(area.TrianguloEq(5.0, 2.0))
+}
